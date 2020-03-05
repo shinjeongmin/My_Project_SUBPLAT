6d6ddd09cefcaa98efb7aa9c19df8e97fd3cc85c
Shin Jeong Min <sin37minduke@naver.com>
2020-03-05 18:56:22 +0900

    PlayerMovement Script Comment

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Controller2D))]
+[RequireComponent(typeof(Controller2D))] 
+// Controller2D 스크립트컴포넌트를 미리 추가한다. 자세히는 모르겠지만 컨트롤러 스크립트의 변수를 가져다 쓰거나 변동할 일이 있는건가?
 public class PlayerMovement : MonoBehaviour
 {
     Controller2D controller; // 플레이어의 움직임에 관여하는 또다른 스크립트
@@ -51,7 +52,7 @@ public class PlayerMovement : MonoBehaviour
 
         gravity = -2 * maxJumpHeight / Mathf.Pow(timeToJumpApex, 2f); // s = 1/2 * a * t^2
         maxJumpVelocity = -gravity * timeToJumpApex; // v = at
-        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight); // v^2 - v0^2= 2as
         doubleJumpVelocity = maxJumpVelocity / (float)1.5;
     }
 
@@ -61,9 +62,11 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -fallingSpeedMax;
 
         if (controller.collisions.below) // 애니메이터 옵션은 나중에 추가할 것
+            //바닥에 충돌했을 때
         {
             jumpcount = 0;
             if ((Input.GetButton("Run") && !crouch) || slide)
+                //슬라이딩 상태일때는 왜 달리는 속도가 움직임속도인가?
             {
                 moveSpeed = runningSpeed;
             }
@@ -75,16 +78,18 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             if (jumpcount == 0)
+                //공중에 있으면 점프카운트 1로 잡아서 2단점프를 공중에서 추가적으로 한 번 할 수 있도록
                 jumpcount = 1;
         }
 
-        wallJumping = (controller.collisions.horizontalTag == "Climbing Wall") ? true : false;
+        wallJumping = (controller.collisions.horizontalTag == "Climbing Wall") ? true : false; //벽타기를 할 수 있는 상태인지 여부를 벽타기 변수에 적용.
 
         float inputXDir = Input.GetAxisRaw("Horizontal"); // 입력한 X축 방향
         
         wallDirX = (controller.collisions.left) ? -1 : 1;
         wallSliding = false;
 
+        // 밀 수 있는 오브젝트에 수평방향으로 충돌상태이지만 물체의 반대방향을 바라볼 때, 밀기 버튼을 누르면 물체쪽을 바라보며 밀기 상태로 변경
         if (controller.collisions.horizontalTag == "Pushable" && controller.collisions.verticalTag != "Pushable"
             && (controller.collisions.left || controller.collisions.right)) {
             if (Input.GetButtonDown("Use"))
@@ -98,6 +103,7 @@ public class PlayerMovement : MonoBehaviour
                 
         }
 
+        //밀기 버튼을 떼면 밀기 상태 취소
         if (pushing)
         {
             if (Input.GetButtonUp("Use"))
@@ -107,7 +113,7 @@ public class PlayerMovement : MonoBehaviour
         }
             
 
-        if (slide) // 슬라이드중이면 좌우 입력이 슬라이드의 경사에 따라서 결정됨
+        if (slide) // 슬라이드중이면 좌우 입력이 슬라이드의 경사에 따라서 결정됨.
         {
             // 플레이어의 X방향 속력
             float slopeXDir = controller.collisions.slopeDir;
@@ -115,13 +121,14 @@ public class PlayerMovement : MonoBehaviour
             // 약간의 가속이 붙도록 smoothdamp를 이용
             velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeSlope);
             // 플레이어의 속도로 애니메이터 설정
-            animator.SetFloat("PlayerSpeed", Mathf.Abs(targetVelocityX));
+            animator.SetFloat("PlayerSpeed", Mathf.Abs(targetVelocityX)); //애니메이터 안에 PlayerSpeed라는 float형 변수가 존재, 이 값에 X방향 속력의 절댓값을 넣는 것.
             if ((slide && !controller.collisions.below && Mathf.Abs(velocity.x) <= Mathf.Abs(runningSpeed)) || (Mathf.Sign(inputXDir) != Mathf.Sign(velocity.x)))
+                // 슬라이딩 상태가 아니고, 달리는 속력보다 속력이 느려지면 더이상 슬라이딩으로 취급하지 않음, 이동방향의 반대방향으로 입력하면 슬라이드가 멈추도록.
                 slide = false;
         }
         else
         {
-            // 플레이어의 X방향 속력
+            // 플레이어의 X방향 속력, 슬라이드가 아닐때는 밀기와 웅크리기속력이 따로 부여된다.
             float targetVelocityX = (crouch || pushing) ? inputXDir * moveSpeed * crouchSpeed : inputXDir * moveSpeed;
             // 약간의 가속이 붙도록 smoothdamp를 이용
             velocity.x = Mathf.SmoothDamp(velocity.x, controlLimit(ref targetVelocityX), ref velocityXSmoothing,
@@ -137,7 +144,7 @@ public class PlayerMovement : MonoBehaviour
             if (velocity.y < -wallSlideSpeedMax) // 일정 속도 이상 낙하하지 않음
                 velocity.y = -wallSlideSpeedMax;
 
-            // 일정 시간동안 벽에 무조건 붙어있도록 함
+            // 일정 시간동안 벽에 무조건 붙어있도록 함 (반대방향 버튼을 눌러도 떨어지지 않는다는 말)
             if (timeToWallUnstick > 0)
             {
                 velocity.x = 0;
@@ -192,7 +199,7 @@ public class PlayerMovement : MonoBehaviour
         // 점프 키를 땠을 때 (점프키를 누른 시간에 따라 점프 높이를 다르게 함)
         if (Input.GetButtonUp("Jump") && !crouch && !slide && !pushing && jumpcount < 2)
         {
-            if (velocity.y > minJumpVelocity)
+            if (velocity.y > minJumpVelocity) // 점프키를 떼는 시점에서 점프할때 가해지는 속력의 크기를 최소화시킴.
             {
                 velocity.y = minJumpVelocity;
                 animator.SetBool("IsJumping", true);
