70fb6934634f81b84a7ba8fc51c5595f705f2c1f
Shin Jeong Min <sin37minduke@naver.com>
2020-03-10 06:24:43 +0900

    Modified Car movement

--- Assets/Scripts/Object/PhysicsCarController.cs
+++ Assets/Scripts/Object/PhysicsCarController.cs
@@ -8,7 +8,7 @@ using System.Collections;
 이동에 관련한 부분은 수정을 자제해주시길 바랍니다.
 */
 
-public class PhysicsCarController : KeyHoleCheck 
+public class PhysicsCarController : KeyHoleCheck
 {
     /* public변수 선언 */
     public float speedF;                //앞바퀴 속도 float변수
@@ -27,6 +27,9 @@ public class PhysicsCarController : KeyHoleCheck
 
     public LayerMask whatIsGround;      // 땅 (자동차가 충돌하면 멈추는) 레이어
 
+    public bool useCar = false;
+    public bool carDirection;
+
     /* private변수 선언 */
     JointMotor2D motorFront;            //앞바퀴 JointMotor2D 변수
     JointMotor2D motorBack;             //뒷바퀴 JointMotor2D 변수
@@ -38,30 +41,40 @@ public class PhysicsCarController : KeyHoleCheck
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    /*FixedUpdate() 함수 정의*/
-    void FixedUpdate()
-    {
-        //지형과 충돌시 정지
-        Vector2 frontVec = new Vector2(rigid.position.x + 1.3f, rigid.position.y);
-        Debug.DrawRay(frontVec, Vector3.right, new Color(0, 1, 0));
-        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.right, 1, whatIsGround);
-
-        if (rayHit.collider != null)
-        {
-            frontwheel.useMotor = false;
-            speedF = 0;
-            torqueF = 0;
-        }
-    }
-    
     /*유틸리티 함수 정의*/
 
     //차와 장애물 충돌 함수
     void OnCollisionEnter2D(Collision2D arg_collision)
     {
         //속도가 있는 상대에서 장애물과 충돌하면 파괴
-        if (arg_collision.gameObject.tag == "Breakable" && motorFront.motorSpeed != 0f)
+        if (arg_collision.gameObject.tag == "Breakable" && speedF > 500)
+        {
+            if (gameObject.transform.position.x < arg_collision.transform.position.x)
+            {
+                attack(-1);
+            }
+            else if (gameObject.transform.position.x > arg_collision.transform.position.x)
+            {
+                attack(1);
+            }
+
             arg_collision.gameObject.GetComponent<BreakableBlock>().ExplodeThisGameObject();
+        }
+    }
+
+    //장애물과 충돌처리 함수
+    void attack(int dir)
+    {
+        rigid.AddForce(new Vector2(dir, 0) * 20, ForceMode2D.Impulse);
+        speedF = 50;
+        torqueF = 100;
+    }
+
+    //가속 함수
+    void Acceleration()
+    {
+        speedF += 25;
+        torqueF += 160;
     }
 
     /*공용 함수 정의*/
@@ -69,24 +82,91 @@ public class PhysicsCarController : KeyHoleCheck
     //차의 이동 함수
     override public void WhenActive()
     {
-        isActive = true;
-        rigid.bodyType = RigidbodyType2D.Dynamic;
-        Move();
+        if (!isActive)
+            isActive = true;
+
+        // rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        if (Mathf.Abs(transform.rotation.z) * Mathf.Rad2Deg <= 35 )
+        {
+            freezeX(false);
+            rigid.bodyType = RigidbodyType2D.Dynamic;
+            useCar = !useCar;
+        }
     }
 
-    public void Move()
+    //차의 이동 함수
+    public void Move(bool direction)
     {
-        if (TractionFront)
+        //모터가 사용중이 아니라면 작동
+        if (!frontwheel.useMotor)
         {
-            motorFront.motorSpeed = speedF * -1;
-            motorFront.maxMotorTorque = torqueF;
-            frontwheel.motor = motorFront;
+            frontwheel.useMotor = true;
+            speedF = 15;
+            torqueF = 100;
         }
-        if (TractionBack)
+
+        if (!direction)
         {
-            motorBack.motorSpeed = speedF * -1;
-            motorBack.maxMotorTorque = torqueF;
-            backwheel.motor = motorBack;
+            if (TractionFront)
+            {
+                motorFront.motorSpeed = speedF * 1;
+                motorFront.maxMotorTorque = torqueF;
+                frontwheel.motor = motorFront;
+            }
+
+            if (TractionBack)
+            {
+                motorBack.motorSpeed = speedF * 1;
+                motorBack.maxMotorTorque = torqueF;
+                backwheel.motor = motorBack;
+
+            }
         }
+        else
+        {
+            if (TractionFront)
+            {
+                motorFront.motorSpeed = speedF * -1;
+                motorFront.maxMotorTorque = torqueF;
+                frontwheel.motor = motorFront;
+            }
+
+            if (TractionBack)
+            {
+                motorBack.motorSpeed = speedF * -1;
+                motorBack.maxMotorTorque = torqueF;
+                backwheel.motor = motorBack;
+
+            }
+        }
+
+        //최고 속도제한
+        if (speedF < 1000 && torqueF < 6666)
+        {
+            if (carDirection && direction || !carDirection && !direction)
+                Acceleration();
+            else
+            {
+                speedF = 1500;
+                torqueF = 1998;
+            }
+        }
+    }
+
+    //차를 정지시키는 함수
+    public void stop()
+    {
+        frontwheel.useMotor = false;
+        speedF = 0;
+        torqueF = 0;
+    }
+
+    public void freezeX(bool freeze)
+    {
+        // freezeX 말고 더 나은 멈춤방법은 없는가?
+        if (freeze)
+            rigid.constraints = RigidbodyConstraints2D.FreezePositionX;
+        else if (!freeze)
+            rigid.constraints = RigidbodyConstraints2D.None;
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Controller2D))] 
-// Controller2D 스크립트컴포넌트를 미리 추가한다. 자세히는 모르겠지만 컨트롤러 스크립트의 변수를 가져다 쓰거나 변동할 일이 있는건가?
+[RequireComponent(typeof(Controller2D))]
+
+/*
+Script_Player_Movement.cs
+
+플레이어의 움직임 (좌우 걷기, 달리기, 점프, 벽차기, 앉기 등등)을 담당하는 스크립트입니다.
+조작감이 향상되는 수치를 찾으면 Prefab의 수치를 변경해 주세요.
+*/
+
 public class PlayerMovement : MonoBehaviour
 {
     Controller2D controller; // 플레이어의 움직임에 관여하는 또다른 스크립트
-    public GameManager gm;
     public Animator animator; // 플레이어의 animator
+    public GameObject car;
+    public GameManager gm;
+    PhysicsCarController carController;
 
     public float maxJumpHeight = 4f; // 점프 높이
     [SerializeField] float minJumpHeight = 1f; // 최소 점프 높이
@@ -18,8 +27,8 @@ public class PlayerMovement : MonoBehaviour
     float doubleJumpVelocity; // 이단 점프할 때 받는 힘
     float gravity; // 플레이어에 작용하는 중력
 
-    [SerializeField] int maxJumpcount = 1; //최대 점프 횟수
     int jumpcount; // 점프 횟수
+    [SerializeField] int maxJumpcount = 1; //최대 점프 횟수
     bool facingRight = true; // 플레이어가 향하고 있는 방향
     bool crouch = false; // 플레이어가 앉아 있는가?
     bool slide = false; // 플레이어가 슬라이딩하는 중인가?
@@ -30,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
     public float runningSpeed = 8f; // 플레이어가 달리는 속도
     public float slidingSpeed = 50f; // 플레이어가 경사로에서 슬라이딩하는 속도
     public float fallingSpeedMax = 20f; // 플레이어의 최대 낙하 속도
+    public float slidingSpeedMax = 50f; // 플레이어의 최대 슬라이딩 속도
     [Range(0, 1)] public float crouchSpeed = .36f; // 앉았을 때 이동 속도 비율
 
     bool wallSliding; // 플레이어가 벽에 붙어있는가?
@@ -50,50 +60,25 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         controller = GetComponent<Controller2D>();
+        carController = car.GetComponent<PhysicsCarController>();
 
         gravity = -2 * maxJumpHeight / Mathf.Pow(timeToJumpApex, 2f); // s = 1/2 * a * t^2
         maxJumpVelocity = -gravity * timeToJumpApex; // v = at
-        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight); // v^2 - v0^2= 2as
-        doubleJumpVelocity = maxJumpVelocity / (float)1.5;
-
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight); // 2as = v^2 - v0^2
+        doubleJumpVelocity = maxJumpVelocity / (float)1.5; // 이단점프 시의 점프력, 기본 점프력의 66%정도임.
     }
 
     void Update()
     {
-        if (velocity.y < -fallingSpeedMax) // 일정 속도 이상 낙하하지 않음
-            velocity.y = -fallingSpeedMax;
+        if (controller.collisions.above || controller.collisions.below) // raycast에서의 중력 축적 방지
+            velocity.y = 0;
 
-        if (controller.collisions.below) // 애니메이터 옵션은 나중에 추가할 것
-            //바닥에 충돌했을 때
-        {
-            jumpcount = 0;
-            if ((Input.GetButton("Run") && !crouch) || slide)
-                //슬라이딩 상태일때는 왜 달리는 속도가 움직임속도인가?
-            {
-                moveSpeed = runningSpeed;
-            }
-            else
-            {
-                moveSpeed = walkingSpeed;
-            }
-        }
-        else
-        {
-            if (jumpcount == 0)
-                //공중에 있으면 점프카운트 1로 잡아서 2단점프를 공중에서 추가적으로 한 번 할 수 있도록
-                jumpcount = 1;
-        }
+        // 미는 상자
+        // 아직 미완성이니 제대로된 작동 기대 X
 
-        wallJumping = (controller.collisions.horizontalTag == "Climbing Wall") ? true : false; //벽타기를 할 수 있는 상태인지 여부를 벽타기 변수에 적용.
-
-        float inputXDir = Input.GetAxisRaw("Horizontal"); // 입력한 X축 방향
-        
-        wallDirX = (controller.collisions.left) ? -1 : 1;
-        wallSliding = false;
-
-        // 밀 수 있는 오브젝트에 수평방향으로 충돌상태이지만 물체의 반대방향을 바라볼 때, 밀기 버튼을 누르면 물체쪽을 바라보며 밀기 상태로 변경
         if (controller.collisions.horizontalTag == "Pushable" && controller.collisions.verticalTag != "Pushable"
-            && (controller.collisions.left || controller.collisions.right)) {
+            && (controller.collisions.left || controller.collisions.right))
+        {
             if (Input.GetButtonDown("Use"))
             {
                 if (controller.collisions.left && facingRight)
@@ -102,43 +87,60 @@ public class PlayerMovement : MonoBehaviour
                     Flip();
                 pushing = true;
             }
-                
-        }
 
-        //밀기 버튼을 떼면 밀기 상태 취소
+        }
         if (pushing)
         {
             if (Input.GetButtonUp("Use"))
             {
                 pushing = false;
-            }   
+            }
         }
-            
 
-        if (slide) // 슬라이드중이면 좌우 입력이 슬라이드의 경사에 따라서 결정됨.
+        float inputXDir = Input.GetAxisRaw("Horizontal"); // 입력한 X축 방향
+
+        if (slide) // 슬라이드중이면 좌우 입력이 슬라이드의 경사에 따라서 결정됨
         {
             // 플레이어의 X방향 속력
             float slopeXDir = controller.collisions.slopeDir;
-            float targetVelocityX = slopeXDir * Mathf.Sin(controller.collisions.slopeAngleCenter * Mathf.Deg2Rad) * slidingSpeed;
-            // 약간의 가속이 붙도록 smoothdamp를 이용
-            velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeSlope);
+
+            float targetVelocityX;
+            if (controller.collisions.below)
+            {
+                targetVelocityX = slopeXDir * Mathf.Sin(controller.collisions.slopeAngleCenter * Mathf.Deg2Rad) * slidingSpeed;
+                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, 
+                    (controller.collisions.onSlope) ? accelerationTimeSlope : accelerationTimeGrounded * 2);
+            }
+            else
+            {
+                targetVelocityX = slopeXDir * Mathf.Sin(controller.collisions.slopeAngleCenter * Mathf.Deg2Rad) * slidingSpeed * 1.5f;
+                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeSlope/*accelerationTimeAirborne*/);
+            }
+
+            if (Mathf.Abs(velocity.x) > slidingSpeedMax && controller.collisions.below)
+                velocity.x = slidingSpeedMax * Mathf.Sign(velocity.x);
+
             // 플레이어의 속도로 애니메이터 설정
-            animator.SetFloat("PlayerSpeed", Mathf.Abs(targetVelocityX)); //애니메이터 안에 PlayerSpeed라는 float형 변수가 존재, 이 값에 X방향 속력의 절댓값을 넣는 것.
-            if ((slide && !controller.collisions.below && Mathf.Abs(velocity.x) <= Mathf.Abs(runningSpeed)) || (Mathf.Sign(inputXDir) != Mathf.Sign(velocity.x)))
-                // 슬라이딩 상태가 아니고, 달리는 속력보다 속력이 느려지면 더이상 슬라이딩으로 취급하지 않음, 이동방향의 반대방향으로 입력하면 슬라이드가 멈추도록.
+            animator.SetFloat("PlayerSpeed", Mathf.Abs(targetVelocityX));
+            if ((slide && !controller.collisions.below && Mathf.Abs(velocity.x) <= Mathf.Abs(runningSpeed)) || (Mathf.Sign(inputXDir) != Mathf.Sign(velocity.x) && inputXDir != 0))
                 slide = false;
         }
         else
         {
-            // 플레이어의 X방향 속력, 슬라이드가 아닐때는 밀기와 웅크리기속력이 따로 부여된다.
+            // 플레이어의 X방향 속력
             float targetVelocityX = (crouch || pushing) ? inputXDir * moveSpeed * crouchSpeed : inputXDir * moveSpeed;
             // 약간의 가속이 붙도록 smoothdamp를 이용
             velocity.x = Mathf.SmoothDamp(velocity.x, controlLimit(ref targetVelocityX), ref velocityXSmoothing,
                 (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
             // 플레이어의 속도로 애니메이터 설정
-            animator.SetFloat("PlayerSpeed", Mathf.Abs(targetVelocityX));
+            if (!carController.useCar)
+                animator.SetFloat("PlayerSpeed", Mathf.Abs(targetVelocityX));
         }
-        
+
+        wallJumping = (controller.collisions.horizontalTag == "Climbing Wall") ? true : false;
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+        wallSliding = false;
+
         // 공중에서 벽에 붙어있고, 떨어지는 때
         if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && wallJumping)
         {
@@ -146,7 +148,7 @@ public class PlayerMovement : MonoBehaviour
             if (velocity.y < -wallSlideSpeedMax) // 일정 속도 이상 낙하하지 않음
                 velocity.y = -wallSlideSpeedMax;
 
-            // 일정 시간동안 벽에 무조건 붙어있도록 함 (반대방향 버튼을 눌러도 떨어지지 않는다는 말)
+            // 일정 시간동안 벽에 무조건 붙어있도록 함
             if (timeToWallUnstick > 0)
             {
                 velocity.x = 0;
@@ -160,10 +162,21 @@ public class PlayerMovement : MonoBehaviour
             else timeToWallUnstick = wallStickTime;
         }
 
-        if (controller.collisions.above || controller.collisions.below) // raycast에서의 중력 축적 방지
-            velocity.y = 0;
- 
-        if ((controller.collisions.below || jumpcount < maxJumpcount) && !pushing && !wallSliding) // 땅에 있을 때
+        if (controller.collisions.below) // 땅에 있을 때
+        {
+            jumpcount = 0; // 점프 카운트를 0으로 초기화
+            if ((Input.GetButton("Run") && !crouch) || slide) // 달릴 수 있는 상황에서 달리기 키를 누를 때
+                moveSpeed = runningSpeed;
+            else
+                moveSpeed = walkingSpeed;
+        }
+        else
+        {
+            if (jumpcount == 0)
+                jumpcount = 1; // 공중에 있다면 떨어지는 순간에도 이단 점프만 가능함
+        }
+
+        if ((controller.collisions.below || jumpcount < maxJumpcount) && !pushing && !wallSliding) // 일반적인 점프를 할 수 있는 상황에서
         {
             if (jumpcount == 0)
                 animator.SetBool("IsJumping", false);
@@ -198,25 +211,6 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        // 점프 키를 땠을 때 (점프키를 누른 시간에 따라 점프 높이를 다르게 함)
-        if (Input.GetButtonUp("Jump") && !crouch && !slide && !pushing && jumpcount < maxJumpcount)
-        {
-            if (velocity.y > minJumpVelocity) // 점프키를 떼는 시점에서 점프할때 가해지는 속력의 크기를 최소화시킴.
-            {
-                velocity.y = minJumpVelocity;
-                animator.SetBool("IsJumping", true);
-            }
-        }
-
-        // 캐릭터의 움직임에 따라 스프라이트의 방향 변경
-        if (inputXDir > 0 && !facingRight && !pushing)
-            Flip();
-        else if (inputXDir < 0 && facingRight && !pushing)
-            Flip();
-
-        velocity.y += gravity * Time.deltaTime; // 중력 작용
-        controller.Move(velocity * Time.deltaTime, crouch); // 이동중 충돌 확인
-
         // 앉기
         if (controller.collisions.below && !pushing && !GetComponent<PlayerInteraction>().haveKey) // 땅에 있을 때
         {
@@ -244,7 +238,7 @@ public class PlayerMovement : MonoBehaviour
                     slide = true;
                 else
                     slide = false;
-            }     
+            }
         }
         else
         {
@@ -254,6 +248,58 @@ public class PlayerMovement : MonoBehaviour
                 animator.SetBool("IsCrouch", false);
             }
         }
+
+        // 점프 키를 땠을 때 (점프키를 누른 시간에 따라 점프 높이를 다르게 함)
+        if (Input.GetButtonUp("Jump") && !crouch && !slide && !pushing && jumpcount < maxJumpcount)
+        {
+            if (velocity.y > minJumpVelocity)
+            {
+                velocity.y = minJumpVelocity;
+                animator.SetBool("IsJumping", true);
+            }
+        }
+
+        // 캐릭터의 움직임에 따라 스프라이트의 방향 변경
+        if (inputXDir > 0 && !facingRight && !pushing)
+            Flip();
+        else if (inputXDir < 0 && facingRight && !pushing)
+            Flip();
+
+        velocity.y += gravity * Time.deltaTime; // 중력 작용
+        if (velocity.y < -fallingSpeedMax) // 일정 속도 이상 낙하하지 않음
+            velocity.y = -fallingSpeedMax;
+
+        if (carController.useCar)
+        { 
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                carController.Move(false);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                carController.stop();
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                carController.Move(true);
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                carController.stop();
+            }
+            if (Input.GetKey(KeyCode.Space) || Mathf.Abs(car.transform.rotation.z * Mathf.Rad2Deg) >= 35)
+            {
+                carController.useCar = false;
+                carController.freezeX(true);
+            }
+            Debug.Log(Mathf.Abs(car.transform.rotation.z * Mathf.Rad2Deg));
+
+            velocity.x = 0;
+            gameObject.transform.position = car.transform.position + new Vector3(0, 1.1f, 0);
+        }
+
+        controller.Move(velocity * Time.deltaTime, crouch); // 이동중 충돌 확인
     }
 
     // 공중에서 좌우 이동 속도 제한
